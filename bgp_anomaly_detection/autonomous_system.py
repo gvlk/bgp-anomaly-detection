c9dda3c8cc22991734a036dd48c5dd61ace1c51d
Gui <88752991+gvlk@users.noreply.github.com>
2024-07-07 18:27:45 -0300

    AS class: minor fixes/changes
    Signed-off-by: Gui <88752991+gvlk@users.noreply.github.com>

--- bgp_anomaly_detection/autonomous_system.py
+++ bgp_anomaly_detection/autonomous_system.py
@@ -1,24 +1,24 @@
 from collections import Counter
+from json import loads
 from typing import Self, Any
 
 
 class AS:
     __slots__ = [
-        "id", "times_seen", "n_mid_path", "n_end_path",
+        "_id", "mid_path_count", "end_path_count",
         "path_sizes", "announced_prefixes", "neighbours"
     ]
 
     def __init__(self, as_id: str) -> None:
         try:
-            int(as_id)
+            id_ = int(as_id)
         except ValueError:
             raise ValueError(f"Invalid AS identifier: '{as_id}' is not a valid integer.")
         else:
-            self.id = str(as_id)
+            self._id = str(id_)
 
-        self.times_seen: int = int()
-        self.n_mid_path: int = int()
-        self.n_end_path: int = int()
+        self.mid_path_count: int = int()
+        self.end_path_count: int = int()
         self.path_sizes: Counter[int] = Counter()
         self.announced_prefixes: set[str] = set()
         self.neighbours: set[str] = set()
@@ -41,14 +41,15 @@ class AS:
             f"neighbours={tuple(self.neighbours)})"
         )
 
-    def __iadd__(self, as_instance: Self) -> Self:
-        if isinstance(as_instance, AS):
-            self.times_seen += as_instance.times_seen
-            self.n_mid_path += as_instance.n_mid_path
-            self.n_end_path += as_instance.n_end_path
-            self.path_sizes.update(as_instance.path_sizes)
-            self.announced_prefixes.update(as_instance.announced_prefixes)
-            self.neighbours.update(as_instance.neighbours)
+    def __iadd__(self, other: Self) -> Self:
+        if isinstance(other, AS):
+            if self.id != other._id:
+                raise RuntimeError(f"These instances do not represent the same AS: {self} | {other}")
+            self.mid_path_count += other.mid_path_count
+            self.end_path_count += other.end_path_count
+            self.path_sizes.update(other.path_sizes)
+            self.announced_prefixes.update(other.announced_prefixes)
+            self.neighbours.update(other.neighbours)
             return self
         return NotImplemented
 
@@ -129,34 +130,27 @@ class AS:
         return {
             "times_seen": self.times_seen,
             "path": {
-                "n_mid_path": self.n_mid_path,
-                "n_end_path": self.n_end_path,
-                "mean_path_size": self.mean_path_size(),
+                "mid_path_count": self.mid_path_count,
+                "end_path_count": self.end_path_count,
+                "mean_path_size": self.mean_path_size,
                 "path_sizes": self.path_sizes
             },
             "prefix": {
-                "total_prefixes": len(self.announced_prefixes),
+                "total_prefixes": self.total_prefixes,
+                "ipv4_count": self.ipv4_count,
+                "ipv6_count": self.ipv6_count,
                 "announced_prefixes": tuple(self.announced_prefixes)
             },
             "neighbour": {
-                "total_neighbours": len(self.neighbours),
+                "total_neighbours": self.total_neighbours,
                 "neighbours": tuple(self.neighbours)
             }
         }
 
     def reset(self):
         """Reset the AS statistics."""
-        self.times_seen = int()
-        self.n_mid_path = int()
-        self.n_end_path = int()
+        self.mid_path_count = int()
+        self.end_path_count = int()
         self.path_sizes.clear()
         self.announced_prefixes.clear()
         self.neighbours.clear()
-
-    def mean_path_size(self) -> float:
-        """Calculate the mean path size for the AS."""
-        total_paths = sum(self.path_sizes.values())
-        if total_paths == 0:
-            return 0.0
-        weighted_sum = sum(int(size) * count for size, count in self.path_sizes.items())
-        return weighted_sum / total_paths
